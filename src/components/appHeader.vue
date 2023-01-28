4eb7ece568ce7fabd8a852d9dafce6b8fc14a288
Urinov Azizbek <uacoding01@gmail.com>
2023-01-28 13:51:12 +0300

    checked and uploaded

---
+++ src/components/appHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="appHeader">
+      <h1 class="btn-primary text-center">{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appHeader',
+  data(){
+      return{
+          title:"Welcome to my simple Voice App "
+      }
+  },
+  methods:{
+      
+  }
+
+}
+</script>
+
+
+<style scoped>
+h1{
+    padding: 25px;
+    font-weight: bold;
+    color:white;
+}
+
+
+
+
+</style>
